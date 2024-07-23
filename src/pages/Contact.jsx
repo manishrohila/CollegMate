1578e3960d56b846640cb54ea77eb448004e5c86
manishrohila <manishrohila0507@gmail.com>
2024-07-23 14:28:47 +0530

    Pull Completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
           <FeedbackForm />
         </div>
       </div>
-      <div className="flex justify-around w-full mt-12 bg-blue-600
+      <div className="flex justify-around w-full mt-12
 bg-blue-600">
         <div className="flex flex-col items-center p-4 rounded-lg">
           <img
